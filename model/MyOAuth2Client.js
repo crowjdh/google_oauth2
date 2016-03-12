b4295fb32f0a14f0b44ad935f5817312d54a5914
Chris Jeong <crowjdh@gmail.com>
2016-03-12 07:13:07 +0000

    change redirect url

--- model/MyOAuth2Client.js
+++ model/MyOAuth2Client.js
@@ -4,6 +4,6 @@ var OAuth2Client = google.auth.OAuth2;
 // https://code.google.com/apis/console
 var CLIENT_ID = '418620888781-ujqsjs4uo4aq74otmr83ph64nhpmkidg.apps.googleusercontent.com';
 var CLIENT_SECRET = 'XJxQ0tGnps2FCUE3stq2uSYe';
-var REDIRECT_URL = 'https://nameless-tor-57749.herokuapp.com';
+var REDIRECT_URL = 'https://nameless-tor-57749.herokuapp.com/users';
 
 module.exports = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
